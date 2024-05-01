663dbdb306a450c9c104d19ac3e028f81a50e603
Osumare1234 <emmanuelosumare@outlook.com>
2024-05-01 03:49:57 -0700

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import About from '@/components/about'
-import Experience from '@/components/experience'
-import Intro from '@/components/intro'
-import Projects from '@/components/projects'
-import SectionDivider from '@/components/section-divider'
-import Skills from '@/components/skills'
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
+import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Intro/>
-    <SectionDivider/>
-    <About/>
-    <Projects/>
-    <Skills/>
-    <Experience/>
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
-  )
+  );
 }
-       
--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Emma CV (2).pdf"
           download
         >
           Download CV{" "}
@@ -98,14 +98,14 @@ export default function intro() {
         </a>
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/emmanuel-osumare-334b11148"
           target="_blank"
         >
           <BsLinkedin />
         </a>
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/Osumare1234 "
           target="_blank"
         >
           <FaGithubSquare />
